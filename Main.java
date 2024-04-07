a566ca37079bb0355dfca316ac8158fec74299cc
ProgrammerDATCH <programmerdatch@gmail.com>
2024-04-07 20:18:24 +0200

    #2

---
+++ Main.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+public class Main {
+    public static void main(String[] args) {
+        JFrame f = new JFrame("Login");
+        f.setSize(800, 500);
+        f.setVisible(true);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
